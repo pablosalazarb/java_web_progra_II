2485bf1c6168d0c92d40e204196cd1a7e5e5a71f
pablouni <pablouni@192.168.58.101>
2022-09-01 22:50:21 -0600

    Se cargan archivos y paquetes para el funcionamiento general del proyecto

---
+++ src/java/classes_package/client.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes_package;
+
+/**
+ *
+ * @author pablouni
+ */
+public class client {
+    private String codigo;
+    private String nombre;
+    private String editorial;
+    private String tipo_pasta;
+    private String publicacion;
+    
+    public client(String codigo, String nombre, String editorial, String tipo_pasta, String publicacion){
+        this.codigo=codigo;
+        this.nombre=nombre;
+        this.editorial=editorial;
+        this.tipo_pasta=tipo_pasta;
+        this.publicacion=publicacion;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getTipo_pasta() {
+        return tipo_pasta;
+    }
+
+    public void setTipo_pasta(String tipo_pasta) {
+        this.tipo_pasta = tipo_pasta;
+    }
+
+    public String getPublicacion() {
+        return publicacion;
+    }
+
+    public void setPublicacion(String publicacion) {
+        this.publicacion = publicacion;
+    }
+    
+}
---
+++ web/scripts/main.js
@@ -0,0 +1,41 @@
+/* 
+ * Archivo controlador del front-end con js
+ * Autor: Juan Pablo Salazar Barrios
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+function sendInformation(){
+    //Creamos un objeto Httprequest.
+    let request = new XMLHttpRequest();
+    //Abrimos la peticion indicandole el metodo de envio y el valor verdadero.
+    request.open('POST' , 'servlet_controller', true);
+    //Configuramos el header de la peticion (que tipo de dato enviaremos, etc)
+    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    
+    //Verificamos el evento de cambio en el estado de la funcion
+    request.onreadystatechange = function(){
+        //Si la peticion se proceso (status done)
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Verificamos si la peticion se proceso correctamente o con error
+            if(request.status === 200){
+                //Mostramos un alert indicando que el proceso fue exitoso
+                swal.fire(
+                    'Succes!',
+                    'Information succed! :D',
+                    'success'
+                );
+            } else{
+                //Mostramos un alert indicando el error!
+                swal.fire(
+                    'Error',
+                    'Oops, we had an error sending your info :(',
+                    'error'
+                );
+            }
+        }
+    };
+    var formData = new FormData(document.getElementById('contact-form'));
+    request.send(formData);
+}
+
